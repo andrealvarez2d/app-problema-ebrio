508fb76072a7de9bb2c646d4259b0e2ee2491799
andrealvarez2d <alvarez.andreabeatriz@gmail.com>
2022-06-13 23:19:28 -0400

    subir

---
+++ app.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El problema del EBRIO</title>
+
+    <style>
+    html {
+        height: 100%;
+    }
+
+    body {
+        height: 100%;
+        background: linear-gradient(to right, #8C579C, #24468E);
+    }
+
+    h1,
+    h2,
+    h3,
+    p {
+        color: white;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>Realizado por: Andrea Álvarez C.I. 27.847.021</h1>
+        <h1>Desarrollar una aplicación que permita resolver el problema del EBRIO</h1>
+        <?php
+        if(isset($_POST['btn']) && $_POST['btn'] == 'Aceptar'){
+
+        $numAciertos = 0; //contador del numero de aciertos
+        $intentos = $_POST['intentos']; //numero de intetos
+        $probabilidad = 0; //variable de la probabilidad
+
+        for($i=1; $i<=$intentos; $i++){
+            $x = 0; //direccion norte-sur
+            $y = 0; //direccion este-oeste
+            $cuadrasCaminadas = 0; //# de cuadras caminadas
+
+            echo '<h2>Intento '.$i.'</h2>';
+
+            while ($cuadrasCaminadas != 10){
+                $r = rand(0,99); //# aleatorio que representa la probabilidad de direccion
+
+                //evaluar la probabilidad de direccion
+                if($r >= 0 AND $r < 25){
+                    $y++;
+                } elseif($r >= 25 AND $r < 50){
+                    $y--;
+                }elseif($r >= 50 AND $r < 75){
+                    $x++;
+                }elseif($r >= 75 AND $r < 100){
+                    $x--;
+                }
+
+                //incrementar el # de cuadras caminadas
+                $cuadrasCaminadas++;
+
+                //mostrar variables posicion X y Y
+                echo '<h3>caminata '.$cuadrasCaminadas.'</h3>';
+                echo '<p>Posición X: '.$x.' Posición Y: '.$y.'</p>';
+                echo '<br>';
+            }
+
+            //sumar la posicion final y comprobar si llego a dos calles
+                $sumaPosicion = $x + $y;
+                if($sumaPosicion == 2 OR $sumaPosicion == -2) {
+                echo '<h3>llego a dos calles</h3>';
+                $numAciertos++;
+            }
+
+            //mostrar posicion final
+            echo '<h3>La posición final es: X: '.$x.', Y: '.$y.'</h3>';
+            echo'<br>';
+            }
+
+            //calcular y mostrar la probabilidad
+            $probabilidad = ($numAciertos/$intentos)*100;
+            echo'<h2>probabilidad: '.number_format($probabilidad, 2).' %</h2>';
+    }
+        ?>
+    </center>
+</body>
+
+</html>
+
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El problema del EBRIO</title>
+    <style>
+    html {
+        height: 100%;
+    }
+
+    body {
+        height: 100%;
+        background: linear-gradient(to right, #8C579C, #24468E);
+    }
+
+    h1,
+    h2,
+    h3,
+    p {
+        color: white;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>Realizado por: Andrea Álvarez C.I. 27.847.021</h1>
+        <h1>Desarrollar una aplicación que permita resolver el problema del EBRIO</h1>
+        <h2>Supongamos que un ebrio está parado en una esquina, cuando decide caminar para que le pase el efecto “ya
+            ustedes
+            saben cuál”. Supongamos que existe una probabilidad igual de que se dirija al norte, sur, este u oeste al
+            llegar
+            a cada esquina. Si camina 10 calles, ¿Cuál es la probabilidad de que termine su recorrido a dos calles de
+            donde
+            lo empezó?
+        </h2>
+
+        <h2>Ingrese número de intentos:</h2>
+        <form action="app.php" method="post">
+            <input type="number" name="intentos" min="1" max="100">
+            <input type="submit" name="btn" value="Aceptar">
+            <input type="reset" name="btn" value="Borrar">
+        </form>
+    </center>
+</body>
+
+</html>
